547f0c2c4598d7f5c4d47e88353f9698007aa76a
Viktor Dohányos <vorkapp@gmail.com>
2019-03-26 18:44:27 +0100

    1

--- ArenaFight/Monsters.cs
+++ ArenaFight/Monsters.cs
@@ -8,7 +8,7 @@ namespace ArenaFight
 {
     class Monsters
     {
-      //private MonsterBuilder Bruti = new MonsterBuilder("Bruti", EvilOrNot, Health, Armor, Strength, Speed, Intelligence);
+      //private MonsterBuilder Bruti = new MonsterBuilder("Bruti", EvilOrNot, Health, Armor, Strength, Speed, Intelligence );
         private MonsterBuilder ElvenGhst = new MonsterBuilder("Elven Ghost Scout", false, 60, 40, 25, 15, 20);
         private MonsterBuilder OrcWarrior = new MonsterBuilder("Weak orc warrior", true, 25, 5, 12, 7, 4);
         private MonsterBuilder OrcShaman = new MonsterBuilder("Orc Shaman", true, 70, 5, 8, 10, 20);
